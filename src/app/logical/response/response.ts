7c1ad22076fe73826ab69b5eeadab3c0fcdbb643
Mauro Di Nardo <m-dinardo08@hotmail.co.uk>
2020-08-17 23:46:12 +0100

    created response wrapper for waiting on Api returns

--- src/app/logical/response/response.ts
+++ src/app/logical/response/response.ts
@@ -1,3 +1,15 @@
-export class Response {
+import { Observable } from 'rxjs';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
+export class Response<T> {
+
+    message: Observable<T>;
+
+    constructor(message: Observable<T>) {
+        this.message = message;
+    }
+
+    respond(func: (dto: T) => void): void {
+        this.message.subscribe(func);
+    }
 }
